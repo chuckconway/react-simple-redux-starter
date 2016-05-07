5f45c163faf69caed2173d4c07fe7607f6a373ba
Chuck Conway <charles@cconway.com>
2016-05-07 10:02:18 -0700

    completed the Redux Sample application

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function (){
     return [
-      {title: 'Javscript: The Good Parts'},
-      {title: 'Harry Potter'},
-      {title: 'The Dark Tower'},
-      {title: 'Eloquent Ruby'}
+      {title: 'Javscript: The Good Parts', pages: 30},
+      {title: 'Harry Potter', pages: 330},
+      {title: 'The Dark Tower', pages: 430},
+      {title: 'Eloquent Ruby', pages: 130}
     ];
 }
